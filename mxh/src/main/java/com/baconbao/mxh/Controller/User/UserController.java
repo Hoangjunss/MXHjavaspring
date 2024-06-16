8997d5883811d851e9c283eec2716cb78bf4dcb7
NKhanh0908 <147810372+NKhanh0908@users.noreply.github.com>
2024-06-16 21:01:48 +0700

    khanh

--- mxh/src/main/java/com/baconbao/mxh/Controller/User/UserController.java
+++ mxh/src/main/java/com/baconbao/mxh/Controller/User/UserController.java
@@ -19,30 +19,45 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    //Nhan trang chu dieu kien la "/"
     @GetMapping("/")
     public String showIndexPage() {
         return "index";
     }
 
+    //Nhan trang edit dieu kien la "/editaccount"
     @GetMapping("/editaccount")
+    //model la phan minh trar ve trang html 
+    //request param la thong tin minh lay duoc sau dau ? cua url
     public String showEditAccountPage(Model model, @RequestParam long id) {
+        //tim user theo id
         User user = userService.findById(id);
+        //chuyen user ve userDTO
         UserDTO userDTO = userService.getUserDTO(user);
+        //tra userdto ve html
         model.addAttribute("userDTO", userDTO);
+        //ten file html
         return "editaccount";
     }
 
+    
     @PostMapping("/editaccount/{id}")
+    //path varriablr la thong tin duoc lay sau dau / cua url
     public String editAccount(@PathVariable Long id, Model model, UserDTO userDTO, BindingResult result) {
         User user = userService.findById(id);
+        //kiem tra email da ton tai hay chua
         if (userService.isEmailExist(userDTO.getEmail())) {
+            //neu ton tai thi tra ve trang editaccount va thong bao loi
             result.rejectValue("email", null, "Email already exists");
             return "editaccount";
         }
+        //chuyen userDTO ve user
         user.setFirstName(userDTO.getFirstName());
         user.setLastName(userDTO.getLastName());
         user.setEmail(userDTO.getEmail());
+        //luu lai user
         userService.saveUser(user);
+        //quay ve trang chu
         return "redirect:/";
     }
 }
