8c2551aa968f92f21c43e87a4a0017792dea7175
Hoangjunss <109050903+Hoangjunss@users.noreply.github.com>
2024-06-16 14:55:04 +0700

    chung

--- mxh/src/main/java/com/baconbao/mxh/Repository/UserRepository.java
+++ mxh/src/main/java/com/baconbao/mxh/Repository/UserRepository.java
@@ -4,9 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.baconbao.mxh.Models.User;
+import java.util.List;
+
 
 @Repository
 
 public interface UserRepository extends JpaRepository<User,Long> {
-    User findByEmail(String email);
+    User  findByEmail(String email);
 }
