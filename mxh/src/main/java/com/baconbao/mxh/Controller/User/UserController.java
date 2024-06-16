9306b9fbcbc18cc6ef995eb6a241e772d389de97
NKhanh0908 <147810372+NKhanh0908@users.noreply.github.com>
2024-06-16 13:21:47 +0700

    update

--- mxh/src/main/java/com/baconbao/mxh/Controller/User/UserController.java
+++ mxh/src/main/java/com/baconbao/mxh/Controller/User/UserController.java
@@ -1,5 +1,6 @@
 package com.baconbao.mxh.Controller.User;
 
+
 public class UserController {
     
 }
--- mxh/src/main/java/com/baconbao/mxh/Services/Service/UserServiceKhanh.java
+++ mxh/src/main/java/com/baconbao/mxh/Services/Service/UserServiceKhanh.java
@@ -1,5 +1,17 @@
 package com.baconbao.mxh.Services.Service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.baconbao.mxh.DTO.UserDTO;
+import com.baconbao.mxh.Models.User;
+
+@Service
 public interface UserServiceKhanh {
-    
+    List<User> fillAll();
+    User findById(long userId);
+    User getUser(UserDTO userDTO);
+    void saveUser(User user);
+    UserDTO getUserDTO(User user);
 }
