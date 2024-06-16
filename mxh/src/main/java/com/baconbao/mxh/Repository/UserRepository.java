d1fa0db8a4d3acc9288ad2990a2d259aea514007
trungkien <145746252+allpedog@users.noreply.github.com>
2024-06-16 13:18:59 +0700

    11

--- mxh/src/main/java/com/baconbao/mxh/Repository/UserRepository.java
+++ mxh/src/main/java/com/baconbao/mxh/Repository/UserRepository.java
@@ -1,8 +1,11 @@
 package com.baconbao.mxh.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.baconbao.mxh.Models.User;
+
+@Repository
 
 public interface UserRepository extends JpaRepository<User,Long> {
     
--- mxh/src/main/java/com/baconbao/mxh/Services/Service/UserService_Kien.java
+++ mxh/src/main/java/com/baconbao/mxh/Services/Service/UserService_Kien.java
@@ -1,5 +1,11 @@
 package com.baconbao.mxh.Services.Service;
 
+import com.baconbao.mxh.Models.User;
+
 public interface UserService_Kien {
+    // tạo tài khoản
+    public User createUser(User user);
+    public boolean isEmailExist(String email);
+    public boolean isNameExist(String name);
     
 }
