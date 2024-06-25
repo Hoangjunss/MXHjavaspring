e98cd283769743f93a172465effcd89a1b1535ef
Hoangjunss <109050903+Hoangjunss@users.noreply.github.com>
2024-06-25 20:30:20 +0700

    chung

--- mxh/src/main/java/com/baconbao/mxh/Repository/ImageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.baconbao.mxh.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.baconbao.mxh.Models.Image;
-
-public interface ImageRepository extends JpaRepository<Image,Long> {
-    
-}
--- mxh/src/main/java/com/baconbao/mxh/Services/Service/ImageService.java
+++
@@ -1,9 +0,0 @@
-package com.baconbao.mxh.Services.Service;
-
-import com.baconbao.mxh.DTO.ImageDTO;
-import com.baconbao.mxh.Models.Image;
-
-public interface ImageService {
-    void saveImage(Image image);
-   
-}
