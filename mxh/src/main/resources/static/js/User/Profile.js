d31d7f7958645ff65b46d7ce78328b9dbb4cda3b
trungkien <145746252+allpedog@users.noreply.github.com>
2024-07-07 17:55:11 +0700

    kiên

--- mxh/src/main/resources/static/js/User/Profile.js
+++ mxh/src/main/resources/static/js/User/Profile.js
@@ -444,14 +444,12 @@ function updateStatusPost(data) {
 }
 
 function updatePostPage(data) {
-    const divContainerPost = document.getElementById('containerpost')
-    const elementPost = document.getElementById('postuserupload');
+    const divContainerPost = document.getElementById('containerpost');
     if (data.posts == null) {
         divContainerPost.innerHTML += `
         <h2 class="complex-effect-text">
             There are currently no posts. Let's share wonderful moments together
         </h2>
-
         `;
     } else {
         data.posts.forEach(post => {
@@ -460,115 +458,107 @@ function updatePostPage(data) {
             const countComment = post.comments ? post.comments.length : 0;
             divContainerPost.innerHTML += `
             <ul class="list-unstyled" id="postuserupload"> 
-                                            <div class="post border-bottom p-3 bg-white w-shadow"> <!-- id ul cua post -->
-                                                <div class="media text-muted pt-3">
-                                                    <!-- Avartar user -->
-                                                    <img src="${imgUser}" alt="Online user" class="mr-3 post-user-image">
-                                                    <form th:action="@{/hidepost}" method="post">
-                                                        <input type="hidden" name="id" value="${post.id}">
-                                                        <button type="submit">Ẩn</button>
-                                                    </form>
-                                                    <div class="media-body pb-3 mb-0 small lh-125">
-                                                        <div class="d-flex justify-content-between align-items-center w-100">
-                                                            <!--Hien thi Name user -->
-                                                            <a href="${post.user.id}" class="text-gray-dark post-user-name">${post.user.firstName} ${post.user.lastName}</a>
-                                                        </div>
-                                                        <!-- Display time update -->
-                                                        <span class="d-block">3 hours ago <i class='bx bx-globe ml-3'></i></span>
-                                                    </div>
-                                                </div>
-                                                <div id="displaycontent">
-                                                        <!-- Content -->
-                                                        <div class="mt-3 ">
-                                                        <p>${post.content}</p>
-                                                    <!--End Content -->
-                                                    <!-- Image cua post -->
-                                                    <div class="d-block mt-3">
-                                                        <img src="${imgPost}" class="post-content" alt="post image">
-                                                    </div>
-                                                    <!-- End Image -->
-                                                    </div>
-                                                </div>
-                                                <!-- End time -->
-                                                <div class="mb-3">
-                                                    <!-- Reactions -->
-                                                    <div class="argon-reaction">
-                                                        <span class="like-btn">
-                                                            <a href="#" class="post-card-buttons" id="reactions"><i class='bx bxs-like mr-2'></i></a>
-                                                            <ul class="reactions-box dropdown-shadow">
-                                                                <li class="reaction reaction-like" data-reaction="Like"><button class="reaction-button" data-reaction-id="1" data-post-id="${post.id}"></button></li>
-                                                                <li class="reaction reaction-love" data-reaction="Love"><button class="reaction-button" data-reaction-id="2" data-post-id="${post.id}"></button></li>
-                                                                <li class="reaction reaction-haha" data-reaction="HaHa"><button class="reaction-button" data-reaction-id="3" data-post-id="${post.id}"></button></li>
-                                                                <li class="reaction reaction-wow" data-reaction="Wow"><button class="reaction-button" data-reaction-id="4" data-post-id="${post.id}"></button></li>
-                                                                <li class="reaction reaction-sad" data-reaction="Sad"><button class="reaction-button" data-reaction-id="5" data-post-id="${post.id}"></button></li>
-                                                                <li class="reaction reaction-angry" data-reaction="Angry"><button class="reaction-button" data-reaction-id="6" data-post-id="${post.id}"></button></li>
-                                                            </ul>
-                                                        </span>
-                                                    </div>
-                                                    
-                                                    <!-- End Reactions -->
-                                                    <button class="post-card-buttons show-comments" onclick="fetchCommentsPost(${post.id})" data-id="${post.id}"><i class='bx bx-message-rounded mr-2'></i>${countComment}</button>
-                                                    <!-- Share -->
-                                                    <div class="dropdown dropup share-dropup">
-                                                        <a href="#" class="post-card-buttons" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                            <i class='bx bx-share-alt mr-2'></i> Share
-                                                        </a>
-                                                        <div class="dropdown-menu post-dropdown-menu">
-                                                            <a href="#" class="dropdown-item">
-                                                                <div class="row">
-                                                                    <div class="col-md-2">
-                                                                        <i class='bx bx-share-alt'></i>
-                                                                    </div>
-                                                                    <div class="col-md-10">
-                                                                        <span>Share Now (Public)</span>
-                                                                    </div>
-                                                                </div>
-                                                            </a>
-                                                            <a href="#" class="dropdown-item">
-                                                                <div class="row">
-                                                                    <div class="col-md-2">
-                                                                        <i class='bx bx-share-alt'></i>
-                                                                    </div>
-                                                                    <div class="col-md-10">
-                                                                        <span>Share...</span>
-                                                                    </div>
-                                                                </div>
-                                                            </a>
-                                                            <a href="#" class="dropdown-item">
-                                                                <div class="row">
-                                                                    <div class="col-md-2">
-                                                                        <i class='bx bx-message'></i>
-                                                                    </div>
-                                                                    <div class="col-md-10">
-                                                                        <span>Send as Message</span>
-                                                                    </div>
-                                                                </div>
-                                                            </a>
-                                                        </div>
-                                                    </div>
-                                                    <!-- End Share -->
-                                                </div>
-                                            </div>
-                                        </ul>
+                <div class="post border-bottom p-3 bg-white w-shadow">
+                    <div class="media text-muted pt-3">
+                        <img src="${imgUser}" alt="Online user" class="mr-3 post-user-image">
+                        <form th:action="@{/hidepost}" method="post">
+                            <input type="hidden" name="id" value="${post.id}">
+                            <button type="submit">Ẩn</button>
+                        </form>
+                        <div class="media-body pb-3 mb-0 small lh-125">
+                            <div class="d-flex justify-content-between align-items-center w-100">
+                                <a href="${post.user.id}" class="text-gray-dark post-user-name">${post.user.firstName} ${post.user.lastName}</a>
+                            </div>
+                            <span class="d-block">3 hours ago <i class='bx bx-globe ml-3'></i></span>
+                        </div>
+                    </div>
+                    <div id="displaycontent">
+                        <div class="mt-3 ">
+                            <p>${post.content}</p>
+                            <div class="d-block mt-3">
+                                <img src="${imgPost}" class="post-content" alt="post image">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <div class="argon-reaction">
+                            <span class="like-btn">
+                                <a href="#" class="post-card-buttons" id="reactions"><i class='bx bxs-like mr-2'></i></a>
+                                <ul class="reactions-box dropdown-shadow">
+                                    <li class="reaction reaction-like" data-reaction="Like"><button class="reaction-button" data-reaction-id="1" data-post-id="${post.id}"></button></li>
+                                    <li class="reaction reaction-love" data-reaction="Love"><button class="reaction-button" data-reaction-id="2" data-post-id="${post.id}"></button></li>
+                                    <li class="reaction reaction-haha" data-reaction="HaHa"><button class="reaction-button" data-reaction-id="3" data-post-id="${post.id}"></button></li>
+                                    <li class="reaction reaction-wow" data-reaction="Wow"><button class="reaction-button" data-reaction-id="4" data-post-id="${post.id}"></button></li>
+                                    <li class="reaction reaction-sad" data-reaction="Sad"><button class="reaction-button" data-reaction-id="5" data-post-id="${post.id}"></button></li>
+                                    <li class="reaction reaction-angry" data-reaction="Angry"><button class="reaction-button" data-reaction-id="6" data-post-id="${post.id}"></button></li>
+                                </ul>
+                            </span>
+                        </div>
+                        <button class="post-card-buttons show-comments-btn" onclick="fetchCommentsPost(${post.id})" data-id="${post.id}"><i class='bx bx-message-rounded mr-2'></i>${countComment}</button>
+                        <div class="dropdown dropup share-dropup">
+                            <a href="#" class="post-card-buttons" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class='bx bx-share-alt mr-2'></i> Share
+                            </a>
+                            <div class="dropdown-menu post-dropdown-menu">
+                                <a href="#" class="dropdown-item">
+                                    <div class="row">
+                                        <div class="col-md-2">
+                                            <i class='bx bx-share-alt'></i>
+                                        </div>
+                                        <div class="col-md-10">
+                                            <span>Share Now (Public)</span>
+                                        </div>
+                                    </div>
+                                </a>
+                                <a href="#" class="dropdown-item">
+                                    <div class="row">
+                                        <div class="col-md-2">
+                                            <i class='bx bx-share-alt'></i>
+                                        </div>
+                                        <div class="col-md-10">
+                                            <span>Share...</span>
+                                        </div>
+                                    </div>
+                                </a>
+                                <a href="#" class="dropdown-item">
+                                    <div class="row">
+                                        <div class="col-md-2">
+                                            <i class='bx bx-message'></i>
+                                        </div>
+                                        <div class="col-md-10">
+                                            <span>Send as Message</span>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="hide-comments" id="commentslist-${post.id}" data-id="${post.id}">
+                        <!-- Comments will be loaded here -->
+                    </div>
+                </div>
+            </ul>
             `;
-        })
+        });
     }
 }
 
 function updateCommentsPost(data, postId) {
-    const divContainerComment = document.getElementById('commentslist');
-    divContainerComment.setAttribute('data-id', postId);
-    if (divContainerComment.style.display === 'flex') {
-        divContainerComment.style.display = 'none';
+    const divContainerComment = document.getElementById(`commentslist-${postId}`);
+    if (divContainerComment.classList.contains('show-comments')) {
+        divContainerComment.classList.remove('show-comments');
+        divContainerComment.classList.add('hide-comments');
+        divContainerComment.style.maxHeight = 0;
         return; // Không thực hiện các hành động khác nếu đã ẩn thẻ
     }
+
     let commentsFrom = `
         <div class="row bootstrap snippets">
             <div class="col-md-12">
                 <div class="comment-wrapper">
                     <div class="panel panel-info">
                         <div class="panel-body">
-                            <ul class="media-list comments-list" id="comments-list">
+                            <ul class="media-list" id="comments-list" style="list-style:none;">
                                 <li class="media comment-form">
                                     <a href="#" class="pull-left">
                                         <img th:src="@{/images/users/user-4.jpg}" alt="" class="img-circle">
@@ -633,5 +623,7 @@ function updateCommentsPost(data, postId) {
     }
 
     divContainerComment.innerHTML = commentsFrom + displayComment;
-    divContainerComment.style.display = 'flex';
+    divContainerComment.classList.remove('hide-comments');
+    divContainerComment.classList.add('show-comments');
+    divContainerComment.style.maxHeight = `${divContainerComment.scrollHeight}px`;
 }
